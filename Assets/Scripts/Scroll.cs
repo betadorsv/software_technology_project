57e6ee81d9812be2fa332b79aea2ce2a1346b797
betadorsv <mr.betran.qn@gmail.com>
2020-06-08 02:45:21 +0700

    Betran

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Player player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = player.transform.position.x;
+        mat.mainTextureOffset = offset * Time.fixedDeltaTime / 0.42f;
+    }
+}
